ae8711321868333aad66bc17e1ba4a5473c3cf52
JeongGod <jeonggu223@gmail.com>
2021-08-10 01:46:50 +0900

    Chapter 4-5 1. 게시글 수정 화면 만들기 및 기능 구현

--- src/main/java/com/jeong/book/springboot/web/IndexController.java
+++ src/main/java/com/jeong/book/springboot/web/IndexController.java
@@ -1,21 +1,35 @@
 package com.jeong.book.springboot.web;
 
+import com.jeong.book.springboot.service.posts.PostsService;
+import com.jeong.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     // 머스테치 스타터 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정이 된다.
     // src/main/resources/templates/index.mustache => index
-
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,10 @@ const main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
     },
     save: function (){
         const data = {
@@ -23,7 +27,28 @@ const main = {
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
-    }
+    },
+    update : function () {
+        const data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        const id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
 };
 
 main.init();
