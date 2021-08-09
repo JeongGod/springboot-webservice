40adc617101d7b3bca7ab8db2b25d1599af9677f
JeongGod <jeonggu223@gmail.com>
2021-08-10 01:27:30 +0900

    Chapter 4-4 1. 게시글 조회 화면 만들기

--- src/main/java/com/jeong/book/springboot/web/IndexController.java
+++ src/main/java/com/jeong/book/springboot/web/IndexController.java
@@ -1,22 +1,16 @@
 package com.jeong.book.springboot.web;
 
-import com.jeong.book.springboot.service.posts.PostsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
-
     // 머스테치 스타터 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정이 된다.
     // src/main/resources/templates/index.mustache => index
+
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index() {
         return "";
     }
 
