87e70c3c30cf2f2e0c87f4a5ed8e5bb35061ac7d
JeongGod <jeonggu223@gmail.com>
2021-08-12 01:56:26 +0900

    Chapter 5-3 1. 구글 로그인 화면 구현

--- src/main/java/com/jeong/book/springboot/web/IndexController.java
+++ src/main/java/com/jeong/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jeong.book.springboot.web;
 
+import com.jeong.book.springboot.config.auth.dto.SessionUser;
 import com.jeong.book.springboot.service.posts.PostsService;
 import com.jeong.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,9 +18,15 @@ public class IndexController {
     // 머스테치 스타터 덕분에 앞의 경로와 뒤의 파일 확장자는 자동으로 지정이 된다.
     // src/main/resources/templates/index.mustache => index
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "";
     }
 
