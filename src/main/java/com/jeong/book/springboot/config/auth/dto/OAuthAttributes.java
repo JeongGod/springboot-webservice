3f210ff7c7d8aef916d7f7e7c2a5ec975db5a013
JeongGod <jeonggu223@gmail.com>
2021-08-13 01:50:58 +0900

    Chapter 5-6 1. OAuth2를 이용하여 Naver 로그인 구현

--- src/main/java/com/jeong/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jeong/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,9 @@ public class OAuthAttributes {
      * OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 합니다.
      */
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -37,6 +40,17 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
